e31219eff25cd537aa5dbaafca897d1db577e1c5
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2023-12-12 21:54:47 -0300

    criando projetinho para treino de interface

---
+++ TreinoJava/src/com/github/raulklahold/treinointerface/ClassificarAves.java
@@ -0,0 +1,8 @@
+package com.github.raulklahold.treinointerface;
+
+public class ClassificarAves extends CaracteristicaGeralAnimal {
+	public void ClassificarAve() {
+		System.out.println("ave tem esse aspecto");
+	}
+
+}
---
+++ TreinoJava/src/com/github/raulklahold/treinointerface/ClassificarMamifero.java
@@ -0,0 +1,8 @@
+package com.github.raulklahold.treinointerface;
+
+public class ClassificarMamifero extends CaracteristicaGeralAnimal {
+	public ClassificarMamifero() {
+		System.out.println(" classificando mamifero: ");
+	}
+
+}
