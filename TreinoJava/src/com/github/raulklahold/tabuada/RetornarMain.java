adafd1ace8d7f62fe514a850154bfe8639cb478c
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2023-12-20 23:01:18 -0300

    att5

--- TreinoJava/src/com/github/raulklahold/tabuada/RetornarMain.java
+++ TreinoJava/src/com/github/raulklahold/tabuada/RetornarMain.java
@@ -7,28 +7,36 @@ import java.util.Scanner;
 public class RetornarMain {
 
 	public static void main(String[] args) {
-		ColetarInfos calculo = new ColetarInfos();
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("Digite um numero para a multiplicação: ");
-        double primeiroNum = scanner.nextDouble();
-          calculo.setNumero1(primeiroNum);
-		
+		try {
+			ColetarInfos calculo = new ColetarInfos();
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.print("Digite um numero para a multiplicação: ");
+	        double primeiroNum = scanner.nextDouble();
+	          calculo.setNumero1(primeiroNum);
+			
 
-		System.out.print("Digite um segundo numero para a multiplicação: ");
-        double segundoNumero = scanner.nextDouble();
-		calculo.setNumero2(segundoNumero);
-		scanner.close();
-		
-		double novoNum = ExecutarTabuada.ExecutarCalculo(calculo);
-		
-		DecimalFormat df = new DecimalFormat("#.00");
-        String novoCapitalFormatado = df.format(novoNum);
-		
-		System.out.println("A multiplicação do numero: "+calculo.getNumero1()+" e "+calculo.getNumero2());
-		System.out.println("Irá resultar no valor: "+novoCapitalFormatado);
-		
-		
+			System.out.print("Digite um segundo numero para a multiplicação: ");
+	        double segundoNumero = scanner.nextDouble();
+			calculo.setNumero2(segundoNumero);
+			scanner.close();
+			
+			double novoNum = ExecutarTabuada.ExecutarCalculo(calculo);
+			
+			DecimalFormat df = new DecimalFormat("#.00");
+	        String novoCapitalFormatado = df.format(novoNum);
+			
+			System.out.println("A multiplicação do numero: "+calculo.getNumero1()+" e "+calculo.getNumero2());
+			System.out.println("Irá resultar no valor: "+novoCapitalFormatado);
+			
+		} catch (ArithmeticException e) {
+		    System.out.println("Erro ao realizar cálculo: divisão por zero.");
+		} catch (NumberFormatException e) {
+		    System.out.println("Por favor, insira um número válido.");
+		} catch (Exception e) {
+		    System.out.println("Ocorreu um erro inesperado: " + e.getMessage());
+		}
+
 
 	}
 
