bc0ed1dd42e2d71dfcf545651900386e08b5f570
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2023-12-20 22:32:28 -0300

    inserção de try cath

--- TreinoJava/src/com/github/raulklahold/calculadorainvestimentos/RetornarMain.java
+++ TreinoJava/src/com/github/raulklahold/calculadorainvestimentos/RetornarMain.java
@@ -6,32 +6,41 @@ import java.util.Scanner;
 public class RetornarMain {
 
 	public static void main(String[] args) {
-		
-		ColetarDados investimento = new ColetarDados();
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("Digite o capital inicial que quer investir: ");
-        double capitalInicial = scanner.nextDouble();
-		investimento.setcapital(capitalInicial);
-		
+		try {
+			ColetarDados investimento = new ColetarDados();
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.print("Digite o capital inicial que quer investir: ");
+	        double capitalInicial = scanner.nextDouble();
+			investimento.setcapital(capitalInicial);
+			
 
-		System.out.print("Digite a Taxa ao mês que o capital irá render: ");
-        double taxaAm = scanner.nextDouble();
-		investimento.setjurosAm(taxaAm);
-		
-		System.out.print("Digite a quantidade de meses que o capital irá render: ");
-        int quantidadeMes = scanner.nextInt();
-		investimento.setMesesInvestidos(quantidadeMes);
-		scanner.close();
-		
-		double novoCapital = ExecutarCalculo.realizarInvestimento(investimento);
-		
-		DecimalFormat df = new DecimalFormat("#.00");
-        String novoCapitalFormatado = df.format(novoCapital);
-		
-		System.out.println("Seu capital investido a: " + investimento.getcapital());
-		System.out.println("Sob uma taxa de: "+investimento.getjurosAm());
-		System.out.println("ira render: "+ novoCapitalFormatado);
+			System.out.print("Digite a Taxa ao mês que o capital irá render: ");
+	        double taxaAm = scanner.nextDouble();
+			investimento.setjurosAm(taxaAm);
+			
+			System.out.print("Digite a quantidade de meses que o capital irá render: ");
+	        int quantidadeMes = scanner.nextInt();
+			investimento.setMesesInvestidos(quantidadeMes);
+			scanner.close();
+			
+			double novoCapital = ExecutarCalculo.realizarInvestimento(investimento);
+			
+			DecimalFormat df = new DecimalFormat("#.00");
+	        String novoCapitalFormatado = df.format(novoCapital);
+			
+			System.out.println("Seu capital investido a: " + investimento.getcapital());
+			System.out.println("Sob uma taxa de: "+investimento.getjurosAm());
+			System.out.println("ira render: "+ novoCapitalFormatado);
+		    
+		} catch (ArithmeticException e) {
+		    System.out.println("Erro ao realizar cálculo: divisão por zero.");
+		} catch (NumberFormatException e) {
+		    System.out.println("Por favor, insira um número válido.");
+		} catch (Exception e) {
+		    System.out.println("Ocorreu um erro inesperado: " + e.getMessage());
+		}
+
 		
 		
 		
