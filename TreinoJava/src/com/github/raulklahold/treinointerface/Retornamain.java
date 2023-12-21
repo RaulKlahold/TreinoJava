cb0e90cbf70fb505587e6e94bda601fa17a9469b
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2023-12-20 23:20:51 -0300

    att 9

--- TreinoJava/src/com/github/raulklahold/treinointerface/Retornamain.java
+++ TreinoJava/src/com/github/raulklahold/treinointerface/Retornamain.java
@@ -3,18 +3,31 @@ package com.github.raulklahold.treinointerface;
 public class Retornamain {
 
 	public static void main(String[] args) {
-		ClassificarAves ave = new ClassificarAves(null);
-		ClassificarMamifero mamifero = new ClassificarMamifero(null);
 		
-		System.out.println("Essa é a classificação e suas habilidades: ");
-		
-		ave.nome = "Papagaio";
-		ave.dormir();
-        ave.fazersom();
+        try {
+        	ClassificarAves ave = new ClassificarAves(null);
+    		ClassificarMamifero mamifero = new ClassificarMamifero(null);
+    		ClassificarRepiteis Reptil = new ClassificarRepiteis(null);
+    		
+    		System.out.println("Essa é a classificação e suas habilidades: ");
+    		
+    		ave.nome = "Papagaio";
+    		ave.dormir();
+            ave.fazersom();
 
-        mamifero.nome = "Cachorro";
-        mamifero.dormir();
-        mamifero.fazersom();
+            mamifero.nome = "Cachorro";
+            mamifero.dormir();
+            mamifero.fazersom();
+            
+            Reptil.nome = "Jacaré";
+            Reptil.dormir();
+            Reptil.fazersom();
+            
+        
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro inesperado: " + e.getMessage());
+        }
+
 	}
 
 }
