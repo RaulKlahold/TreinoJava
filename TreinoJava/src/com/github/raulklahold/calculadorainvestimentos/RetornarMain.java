0380bd89d24b5f19e22e1ace946d5c18347ecbd5
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2023-12-20 22:55:31 -0300

    att3

--- TreinoJava/src/com/github/raulklahold/calculadorainvestimentos/RetornarMain.java
+++ TreinoJava/src/com/github/raulklahold/calculadorainvestimentos/RetornarMain.java
@@ -9,42 +9,38 @@ public class RetornarMain {
 		try {
 			ColetarDados investimento = new ColetarDados();
 			Scanner scanner = new Scanner(System.in);
-			
+
 			System.out.print("Digite o capital inicial que quer investir: ");
-	        double capitalInicial = scanner.nextDouble();
+			double capitalInicial = scanner.nextDouble();
 			investimento.setcapital(capitalInicial);
-			
 
 			System.out.print("Digite a Taxa ao mês que o capital irá render: ");
-	        double taxaAm = scanner.nextDouble();
+			double taxaAm = scanner.nextDouble();
 			investimento.setjurosAm(taxaAm);
-			
+
 			System.out.print("Digite a quantidade de meses que o capital irá render: ");
-	        int quantidadeMes = scanner.nextInt();
+			int quantidadeMes = scanner.nextInt();
 			investimento.setMesesInvestidos(quantidadeMes);
 			scanner.close();
-			
+
 			double novoCapital = ExecutarCalculo.realizarInvestimento(investimento);
-			
+
 			DecimalFormat df = new DecimalFormat("#.00");
-	        String novoCapitalFormatado = df.format(novoCapital);
-			
+			String novoCapitalFormatado = df.format(novoCapital);
+
 			System.out.println("Seu capital investido a: " + investimento.getcapital());
-			System.out.println("Sob uma taxa de: "+investimento.getjurosAm());
-			System.out.println("ira render: "+ novoCapitalFormatado);
-		    
+			System.out.println("Sob uma taxa de: " + investimento.getjurosAm());
+			System.out.println("ira render: " + novoCapitalFormatado);
+
 		} catch (ArithmeticException e) {
-			
-		    System.out.println("Erro ao realizar cálculo: não é possivel divisão por zero.");
+
+			System.out.println("Erro ao realizar cálculo: não é possivel divisão por zero.");
 		} catch (NumberFormatException e) {
-		    System.out.println("Erro ao realizar cálculo: Por favor, insira um número válido.");
+			System.out.println("Erro ao realizar cálculo: Por favor, insira um número válido.");
 		} catch (Exception e) {
-		    System.out.println("Erro ao realizar cálculo: Ocorreu um erro inesperado: " + e.getMessage());
+			System.out.println("Erro ao realizar cálculo: Ocorreu um erro inesperado " + e.getMessage());
 		}
 
-		
-		
-		
 	}
 
 }
