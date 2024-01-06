ec0af7a4279f3e0c00704016f47723b4d10adbb2
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-06 12:53:57 -0300

    att dos dados

--- TreinoJava/src/com/github/raulklahold/sistemaderestaurante/ColetarDados.java
+++ TreinoJava/src/com/github/raulklahold/sistemaderestaurante/ColetarDados.java
@@ -3,6 +3,15 @@ package com.github.raulklahold.sistemaderestaurante;
 public class ColetarDados {
 	private String nome;
 	private String cpf;
+	private char pedido;
+
+	public char getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(char pedido) {
+		this.pedido = pedido;
+	}
 
 	public String getNome() {
 		return nome;
--- TreinoJava/src/com/github/raulklahold/sistemaderestaurante/Executarfuncao.java
+++ TreinoJava/src/com/github/raulklahold/sistemaderestaurante/Executarfuncao.java
@@ -3,32 +3,34 @@ package com.github.raulklahold.sistemaderestaurante;
 
 public class Executarfuncao {
 	
-	public static String Pedidoslist(ColetarDados ColetarDados){}
+	public static String Pedidoslist(ColetarDados ColetarDados){
 	
 	 String nome = ColetarDados.getNome();
 	 String CPF = ColetarDados.getCpf();
 	 
 	 switch (pedido) {
-     case "Pedido A":
+     case '1':
          System.out.println("Nome: " + nome);
          System.out.println("CPF: " + CPF);
          System.out.println("Pedido escolhido: " + pedido);
          // Aqui você pode adicionar qualquer lógica específica para o "Pedido A" se necessário
          break;
-     case "Pedido B":
+     case '2':
          System.out.println("Nome: " + nome);
          System.out.println("CPF: " + CPF);
          System.out.println("Pedido escolhido: " + pedido);
          // Lógica específica para o "Pedido B" se necessário
          break;
-     case "Pedido C":
+     case '3':
          System.out.println("Nome: " + nome);
          System.out.println("CPF: " + CPF);
          System.out.println("Pedido escolhido: " + pedido);
          // Lógica específica para o "Pedido C" se necessário
          break;
      default:
+    	 
          System.out.println("Pedido não reconhecido.");
          break;
-	
-}
+	 }
+         return pedido;
+}}
