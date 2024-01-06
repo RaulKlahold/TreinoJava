eac694c97a0c3e47b2f9a7697a6e5ec2bd418be1
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-06 15:31:16 -0300

    atualização da camada main

--- TreinoJava/src/com/github/raulklahold/sistemaderestaurante/RetornaMain.java
+++ TreinoJava/src/com/github/raulklahold/sistemaderestaurante/RetornaMain.java
@@ -1,9 +1,28 @@
 package com.github.raulklahold.sistemaderestaurante;
 
+import java.util.Scanner;
+
 public class RetornaMain {
 
 	public static void main(String[] args) {
-		
+		Scanner Scan = new Scanner(System.in);
+		ColetarDados cliente = new ColetarDados();
+
+		System.out.println("Por favor digite seu nome: ");
+		String nome = Scan.nextLine();
+		cliente.setNome(nome);
+
+		System.out.println("Por favor digite seu CPF: ");
+		String CPf = Scan.nextLine();
+		cliente.setCpf(CPf);
+
+		System.out.println("Por favor digite o numero do seu pedido: ");
+		String pedido = Scan.nextLine();
+		cliente.setPedido(pedido);
+		Scan.close();
+
+		String novopedido = Executarfuncao.Pedidoslist(cliente);
+		System.out.println("\n Muito obrigado pela preferência...\nseu pedido foi o combo:\n"+novopedido );
 
 	}
 
