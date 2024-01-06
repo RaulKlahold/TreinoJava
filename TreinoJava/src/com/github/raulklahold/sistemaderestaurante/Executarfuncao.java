595726a3aee5bc4fc56fb01b533ef4d29ac8ac13
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-06 15:40:11 -0300

    att cardapio

--- TreinoJava/src/com/github/raulklahold/sistemaderestaurante/Executarfuncao.java
+++ TreinoJava/src/com/github/raulklahold/sistemaderestaurante/Executarfuncao.java
@@ -18,20 +18,20 @@ public class Executarfuncao {
          System.out.println("Pedido N°1  \n XBacon duplo com fritas \n Lata Coca Brind\n ");
          break;
      case "2":
-    	 System.out.println("\nNome: " + nome+"\n");
-         System.out.println("\nCPF: " + CPF+"\n");
-         System.out.println("Pedido escolhido: " + pedido+"\n");
+    	 System.out.println("\nNome: " + nome);
+         System.out.println("\nCPF: " + CPF);
+         System.out.println("\nPedido escolhido: " + pedido);
          System.out.println("Pedido N°2  \n XSalada duplo com fritas \n Lata fanta Brind\n ");
          break;
      case "3":
-    	 System.out.println("\nNome: " + nome+"\n");
-         System.out.println("\nCPF: " + CPF+"\n");
-         System.out.println("Pedido escolhido: " + pedido+"\n");
-         System.out.println("Pedido N°3  \n XEgg duplo com fritas \n Lata Sprite Brind\n ");
+    	 System.out.println("\n Nome: " + nome);
+         System.out.println("\n CPF: " + CPF);
+         System.out.println("\n Pedido escolhido: " + pedido);
+         System.out.println("\n Pedido N°3  \n XEgg duplo com fritas \n Lata Sprite Brind\n ");
          break;
      default:
     	 
-         System.out.println("Pedido não reconhecido.");
+         System.out.println("Pedido não encontrado");
          break;
 	 }
          return pedido;
