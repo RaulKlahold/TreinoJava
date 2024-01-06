12304fe1f55b0165b0ff00601cf6e9de9b0d10cb
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-06 12:31:16 -0300

    attt das variavies

--- TreinoJava/src/com/github/raulklahold/sistemaderestaurante/ColetarDados.java
+++ TreinoJava/src/com/github/raulklahold/sistemaderestaurante/ColetarDados.java
@@ -1,6 +1,23 @@
 package com.github.raulklahold.sistemaderestaurante;
 
 public class ColetarDados {
-	String nome;
-    String cpf;
+	private String nome;
+	private String cpf;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
 }
