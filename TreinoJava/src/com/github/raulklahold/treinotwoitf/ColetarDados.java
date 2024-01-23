531e16e7dd736de10bda0b5b010612c504998129
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-22 22:11:01 -0300

    alteração dos dados

--- TreinoJava/src/com/github/raulklahold/treinotwoitf/ColetarDados.java
+++ TreinoJava/src/com/github/raulklahold/treinotwoitf/ColetarDados.java
@@ -1,30 +1,25 @@
 package com.github.raulklahold.treinotwoitf;
 
 public class ColetarDados {
- private double nome;
- private double primeironumero;
- private double segundonumero;
- public PersonaUM persona;
- 
- 
-public double getNome() {
-	return nome;
-}
-public void setNome(double nome) {
-	this.nome = nome;
-}
-public double getPrimeironumero() {
-	return primeironumero;
-}
-public void setPrimeironumero(double primeironumero) {
-	this.primeironumero = primeironumero;
-}
-public double getSegundonumero() {
-	return segundonumero;
-}
-public void setSegundonumero(double segundonumero) {
-	this.segundonumero = segundonumero;
-}
- 
+
+	private double primeironumero;
+	private double segundonumero;
+	public PersonaUM persona;
+
+	public double getPrimeironumero() {
+		return primeironumero;
+	}
+
+	public void setPrimeironumero(double primeironumero) {
+		this.primeironumero = primeironumero;
+	}
+
+	public double getSegundonumero() {
+		return segundonumero;
+	}
+
+	public void setSegundonumero(double segundonumero) {
+		this.segundonumero = segundonumero;
+	}
 
 }
--- TreinoJava/src/com/github/raulklahold/treinotwoitf/PersonaUM.java
+++ TreinoJava/src/com/github/raulklahold/treinotwoitf/PersonaUM.java
@@ -1,5 +1,5 @@
 package com.github.raulklahold.treinotwoitf;
 
 public class PersonaUM {
-
+     
 }
