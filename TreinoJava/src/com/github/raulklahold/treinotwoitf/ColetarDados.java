ff924b465f60099ae09861fc6ef7c06b316c23b3
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-22 22:04:23 -0300

    anexando dados com seus getter e setters

--- TreinoJava/src/com/github/raulklahold/treinotwoitf/ColetarDados.java
+++ TreinoJava/src/com/github/raulklahold/treinotwoitf/ColetarDados.java
@@ -1,5 +1,29 @@
 package com.github.raulklahold.treinotwoitf;
 
 public class ColetarDados {
+ private double nome;
+ private double primeironumero;
+ private double segundonumero;
+ 
+ 
+public double getNome() {
+	return nome;
+}
+public void setNome(double nome) {
+	this.nome = nome;
+}
+public double getPrimeironumero() {
+	return primeironumero;
+}
+public void setPrimeironumero(double primeironumero) {
+	this.primeironumero = primeironumero;
+}
+public double getSegundonumero() {
+	return segundonumero;
+}
+public void setSegundonumero(double segundonumero) {
+	this.segundonumero = segundonumero;
+}
+ 
 
 }
