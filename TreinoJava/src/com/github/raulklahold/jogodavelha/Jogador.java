ac05e0995dcaf4422c1e5cf8a741ecee0de0f7fb
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-29 22:16:46 -0300

    refactor code

---
+++ TreinoJava/src/com/github/raulklahold/jogodavelha/Jogador.java
@@ -0,0 +1,15 @@
+package com.github.raulklahold.jogodavelha;
+
+public class Jogador {
+	 
+            private char simbolo;
+
+		    public Jogador(char simbolo) {
+		        this.simbolo = simbolo;
+		    }
+
+		    public char getSimbolo() {
+		        return simbolo;
+		    }
+		}
+
