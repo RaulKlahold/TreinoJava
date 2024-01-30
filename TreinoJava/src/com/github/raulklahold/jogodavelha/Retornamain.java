7bef31990faacd6f005e1f7d6629c42b97d946a9
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-29 22:17:32 -0300

    refactor code

--- TreinoJava/src/com/github/raulklahold/jogodavelha/Retornamain.java
+++ TreinoJava/src/com/github/raulklahold/jogodavelha/Retornamain.java
@@ -3,7 +3,7 @@ package com.github.raulklahold.jogodavelha;
 public class Retornamain {
 
     public static void main(String[] args) {
-        jogo jogo = new jogo();
+        Jogo jogo = new Jogo();
         jogo.jogar();
     }
 }
