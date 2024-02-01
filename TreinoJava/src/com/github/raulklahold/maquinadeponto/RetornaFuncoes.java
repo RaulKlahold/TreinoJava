933cd96360eedded910f8e2e344d928f81dab7d8
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-02-01 19:48:08 -0300

    mudando tudo do zero

--- TreinoJava/src/com/github/raulklahold/maquinadeponto/RetornaFuncoes.java
+++ TreinoJava/src/com/github/raulklahold/maquinadeponto/RetornaFuncoes.java
@@ -1,23 +1,15 @@
 package com.github.raulklahold.maquinadeponto;
-import java.time.LocalTime;
-import java.time.Duration;
+
 
 public class RetornaFuncoes {
 	
-	
+	public static double HorarioDeTrabalho(CapitacaoDados CapitacaoDados) {
 
 	   
-	        LocalTime hora1 = LocalTime.of(10, 30, 0); 
-	        LocalTime hora2 = LocalTime.of(14, 45, 30);
-
 	        
-	        Duration diferenca = Duration.between(hora1, hora2);
-
-	       
-	        long horas = diferenca.toHours();
 	     
 	        
-	        
+	}
 	    
 	}
 
