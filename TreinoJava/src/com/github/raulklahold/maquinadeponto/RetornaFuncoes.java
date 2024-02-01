c72e3b85a29c06deb107983a81267ee36b27e3e1
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-02-01 19:51:11 -0300

    setando as variavies

--- TreinoJava/src/com/github/raulklahold/maquinadeponto/RetornaFuncoes.java
+++ TreinoJava/src/com/github/raulklahold/maquinadeponto/RetornaFuncoes.java
@@ -7,6 +7,9 @@ public class RetornaFuncoes {
 
 	    double hora1 = CapitacaoDados.getHorarioentrada();
 	    double hora2 = CapitacaoDados.getHorariosaida();
+	    
+	    double diferença = hora2 - hora1;
+	    
 	        
 	     
 	        
