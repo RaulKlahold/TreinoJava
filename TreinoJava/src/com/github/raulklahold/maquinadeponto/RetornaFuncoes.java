e33241f40e3e8d2e961c4d22b0b680bb6a35f872
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-02-01 19:43:15 -0300

    arrumando o codgio das functions

--- TreinoJava/src/com/github/raulklahold/maquinadeponto/RetornaFuncoes.java
+++ TreinoJava/src/com/github/raulklahold/maquinadeponto/RetornaFuncoes.java
@@ -7,14 +7,13 @@ public class RetornaFuncoes {
 	
 
 	   
-	        // Criar duas instâncias de LocalTime
-	        LocalTime hora1 = LocalTime.of(10, 30, 0); // 10:30:00
-	        LocalTime hora2 = LocalTime.of(14, 45, 30); // 14:45:30
+	        LocalTime hora1 = LocalTime.of(10, 30, 0); 
+	        LocalTime hora2 = LocalTime.of(14, 45, 30);
 
-	        // Calcular a diferença entre as duas horas
+	        
 	        Duration diferenca = Duration.between(hora1, hora2);
 
-	        // Exibir a diferença em horas, minutos e segundos
+	       
 	        long horas = diferenca.toHours();
 	        long minutos = diferenca.toMinutesPart();
 	        long segundos = diferenca.toSecondsPart();
