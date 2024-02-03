226274ba74187855b20e4eb7e734783ff15118b2
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-02-02 21:02:24 -0300

    setando as infos da main

--- TreinoJava/src/com/github/raulklahold/maquinadeponto/RetornaMain.java
+++ TreinoJava/src/com/github/raulklahold/maquinadeponto/RetornaMain.java
@@ -3,12 +3,20 @@ package com.github.raulklahold.maquinadeponto;
 import java.util.Scanner;
 
 public class RetornaMain {
+	public static void main(String[] args) {
+		
 	Scanner scan = new Scanner(System.in);
 	CapitacaoDados horario = new CapitacaoDados();
 	
+	System.out.print("Digite seu nome: ");
+	String nome = scan.next();
+	horario.setNome(nome);
+
 	
 	
 	
 	
+	
+	}
 
 }
