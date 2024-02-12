6dbbe3f2e09d103b3ee1e0eee7b01583ed30ffd1
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-02-12 13:07:08 -0300

    tentando arrumar o desafio

--- TreinoJava/src/com/github/raulklahold/desfio/RetornaMain.java
+++ TreinoJava/src/com/github/raulklahold/desfio/RetornaMain.java
@@ -7,7 +7,8 @@ public class RetornaMain {
 	 
 	public static  void main(String[] args) {
 	        GerenciadorTemperaturas gerenciador = new GerenciadorTemperaturas();
-
+            final int tamanho = 5;
+            double soma = 0;
 	      
 	        gerenciador.adicionarTemperatura(20.5);
 	        gerenciador.adicionarTemperatura(15.7);
@@ -21,7 +22,7 @@ public class RetornaMain {
 
 	        
 	        for (temperatura temp : gerenciador.getTemperaturas()) {
-	        	
+	        	if(temp> temp)
 	            System.out.println(temp);
 	            
 	            
