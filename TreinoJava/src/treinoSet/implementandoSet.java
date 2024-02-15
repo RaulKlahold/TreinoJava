711bc2b9a3223ee821743ba172fabdd7f8b270ec
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-02-15 19:48:43 -0300

    treino set

--- TreinoJava/src/treinoSet/implementandoSet.java
+++ TreinoJava/src/treinoSet/implementandoSet.java
@@ -1,10 +1,14 @@
 package treinoSet;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class implementandoSet {
 
 	public static void main(String[] args) {
 		Classepessoa pessoa= new Classepessoa(null);
-
+		Set<String> colecaoSet = new HashSet<String>();
+		
 	}
 
 }
