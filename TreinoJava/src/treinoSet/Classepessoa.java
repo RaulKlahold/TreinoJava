4d0313ec4ac11706ad060f443909abaa88f47b2c
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-02-15 19:47:20 -0300

    treinando set

--- TreinoJava/src/treinoSet/Classepessoa.java
+++ TreinoJava/src/treinoSet/Classepessoa.java
@@ -1,5 +1,43 @@
 package treinoSet;
 
-public class Classepessoa {
+import java.util.Objects;
+
+public class Classepessoa  {
+private String nome;
+
+public String getNome() {
+	return nome;
+}
+
+public Classepessoa(String nome) {
+	super();
+	this.nome = nome;
+}
+
+@Override
+public String toString() {
+	return "Classepessoa [nome=" + nome + "]";
+}
+
+@Override
+public int hashCode() {
+	return Objects.hash(nome);
+}
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Classepessoa other = (Classepessoa) obj;
+	return Objects.equals(nome, other.nome);
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
 
 }
--- TreinoJava/src/treinoSet/implementandoSet.java
+++ TreinoJava/src/treinoSet/implementandoSet.java
@@ -3,7 +3,7 @@ package treinoSet;
 public class implementandoSet {
 
 	public static void main(String[] args) {
-		
+		Classepessoa pessoa= new Classepessoa(null);
 
 	}
 
