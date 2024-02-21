a21f55c08fb0c9cb8c48d3d0b4dd6682b16e7328
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-02-20 22:26:48 -0300

    ultimas alterações

--- TreinoJava/src/com/github/raulklahold/treinoStream/RetornaMain.java
+++ TreinoJava/src/com/github/raulklahold/treinoStream/RetornaMain.java
@@ -8,17 +8,23 @@ public class RetornaMain {
 	public static void main(String[] args) {
 		List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
 		/*
-		 * Array.asList inicia uma lista com valores prédefinidos, no qual não podemos
-		 * adicionar elementos na lista, se quiser ter esse poder de manipulação tem que
+		 * Array.asList inicia uma lista com valores prédefinidos, 
+		 * no qual não podemos
+		 * adicionar elementos na lista, se quiser ter esse poder
+		 *  de manipulação tem que
 		 * setar uma nova list: List<Integer> lista = new ArrayList<>();
 		 * 
 		 */
 
-		int somaDosParesDobrados = numeros.stream().filter(n -> n > 1).mapToInt(n -> (n * 2) / 2)
-				.sum(); /*
-						 * O ponto e vírgula é colocado apenas após sum() porque é o método de terminal
-						 * que finaliza a operação do Stream
-						 */
+		int somaDosParesDobrados = numeros.stream()
+				.filter(n -> n > 1)
+				.mapToInt(n -> (n * 2) / 2)
+				.sum(); 
+		/*
+		* O ponto e vírgula é colocado apenas após sum()
+		*  porque é o método de terminal
+		* que finaliza a operação do Stream
+			*/
 
 		System.out.println("A soma deu: " + somaDosParesDobrados);
 
