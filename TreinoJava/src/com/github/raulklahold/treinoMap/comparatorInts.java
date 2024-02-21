4e0caca2cbb97e0f6caee8e5caaad3f3c5eae2c5
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-02-20 22:04:38 -0300

    aletração treinamento map

--- TreinoJava/src/com/github/raulklahold/treinoMap/comparatorInts.java
+++ TreinoJava/src/com/github/raulklahold/treinoMap/comparatorInts.java
@@ -1,26 +1,2 @@
-package com.github.raulklahold.treinoMap;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
 
-public class comparatorInts {
-
-	 public comparatorInts(HashMap<Integer, String> mapaNomes) {
-		
-
-	public class ComparatorInts implements Comparator<integer>{
-		 
-		 private Map<integer string=""> base;
-
-		  public ComparatorInts(Map<integer, string=""> base) {
-		    this.base = base;
-		  }
-
-		  @Override
-		  public int compare(Integer o1, Integer o2) {
-		    return base.get(o1).compareTo(base.get(o2));
-		  }
-		 
-	 }
-}}
