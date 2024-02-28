7f65b81880d1d25b73f8ff2698c622167fc53683
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-02-28 20:59:41 -0300

    anexando as varivesi de stream através do scanner

--- TreinoJava/src/com/github/raulklahold/treinoStream/RetornaMain.java
+++ TreinoJava/src/com/github/raulklahold/treinoStream/RetornaMain.java
@@ -8,34 +8,32 @@ public class RetornaMain {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.println("Digite um numero: ");
 		int var1 = scan.nextInt();
-		
+
 		System.out.println("Digite um outro numero: ");
 		int var2 = scan.nextInt();
-		
+
 		System.out.println("Digite um outro numero: ");
 		int var3 = scan.nextInt();
-		
+
 		System.out.println("Digite um outro numero: ");
 		int var4 = scan.nextInt();
-	
+
 		System.out.println("Digite um outro numero: ");
 		int var5 = scan.nextInt();
-		
+
 		System.out.println("Digite um outro numero: ");
 		int var6 = scan.nextInt();
-		
+
 		System.out.println("Digite um outro numero: ");
 		int var7 = scan.nextInt();
-		
+
 		List<Integer> numeros = Arrays.asList(var1, var2, var3, var4, var5, var6, var7);
 		/*
-		 * Array.asList inicia uma lista com valores prédefinidos, 
-		 * no qual não podemos
-		 * adicionar elementos na lista, se quiser ter esse poder
-		 *  de manipulação tem que
+		 * Array.asList inicia uma lista com valores prédefinidos, no qual não podemos
+		 * adicionar elementos na lista, se quiser ter esse poder de manipulação tem que
 		 * setar uma nova list: List<Integer> lista = new ArrayList<>();
 		 * 
 		 */
@@ -43,12 +41,13 @@ public class RetornaMain {
 		int somaDosParesDobrados = numeros.stream()
 				.filter(n -> n > 2)
 				.mapToInt(n -> (n * 2) / 2)
-				.sum(); 
+				.sum();
+		
+		
 		/*
-		* O ponto e vírgula é colocado apenas após sum()
-		*  porque é o método de terminal
-		* que finaliza a operação do Stream
-			*/
+		 * O ponto e vírgula é colocado apenas após sum() porque é o método de terminal
+		 * que finaliza a operação do Stream
+		 */
 
 		System.out.println("A soma deu: " + somaDosParesDobrados);
 
