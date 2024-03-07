c55739ce21d681b79ff8279f5b899310e7e0eca2
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-06 22:39:36 -0300

    quarto commit

--- TreinoJava/src/com/github/raulklahold/treinodeQuadrinho/QuadroUsuario.java
+++ TreinoJava/src/com/github/raulklahold/treinodeQuadrinho/QuadroUsuario.java
@@ -6,8 +6,14 @@ public class QuadroUsuario {
 
 	public static void main(String[] args) {
 		
-		String numero1 = JOptionPane.showInputDialog("Qual é o seu nome?");
-		String numero2 = JOptionPane.showInputDialog("Qual é o seu nome?");
+		String numero1 = JOptionPane.showInputDialog("Qual é o seu primeiro numero?");
+		String numero2 = JOptionPane.showInputDialog("Qual é o seu segundo numero?");
+		
+		double numeroUm = Double.parseDouble(numero1);
+		double numeroDois = Double.parseDouble(numero2);
+		
+		double divisão = numeroUm / numeroDois;
+		System.out.println();
 	}
 
 }
