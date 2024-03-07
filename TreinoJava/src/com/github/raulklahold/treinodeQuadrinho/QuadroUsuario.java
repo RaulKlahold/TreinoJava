f62e8cd078e2c3955a5a608f823ba01dec4cf3a6
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-06 22:36:17 -0300

    terceiro commit

--- TreinoJava/src/com/github/raulklahold/treinodeQuadrinho/QuadroUsuario.java
+++ TreinoJava/src/com/github/raulklahold/treinodeQuadrinho/QuadroUsuario.java
@@ -1,10 +1,13 @@
 package com.github.raulklahold.treinodeQuadrinho;
 
+import javax.swing.JOptionPane;
+
 public class QuadroUsuario {
 
 	public static void main(String[] args) {
 		
-
+		String numero1 = JOptionPane.showInputDialog("Qual é o seu nome?");
+		String numero2 = JOptionPane.showInputDialog("Qual é o seu nome?");
 	}
 
 }
