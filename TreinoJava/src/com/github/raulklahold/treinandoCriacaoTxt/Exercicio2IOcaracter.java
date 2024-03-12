978531f189f0d03cfd9ea445c65b071423d23f7c
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-11 22:59:31 -0300

    andamento do treino

--- TreinoJava/src/com/github/raulklahold/treinandoCriacaoTxt/Exercicio2IOcaracter.java
+++ TreinoJava/src/com/github/raulklahold/treinandoCriacaoTxt/Exercicio2IOcaracter.java
@@ -1,17 +1,22 @@
 package com.github.raulklahold.treinandoCriacaoTxt;
 
 import java.io.*;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Exercicio2IOcaracter {
 
 	public static void lerTecladoEscreverDocumento()throws IOException{
 		System.out.println("Digite 3 recomendações de filmes: ");
+		
 		Scanner scan = new Scanner(System.in);
 		
 		String line = scan.nextLine();
 		
 	    File f = new File("recomendações.txt");
+	    BufferedWriter bw = new BufferedWriter(new FileWriter(f.getName()));
+	    
+	    do {
+	    	
+	    }
 	}
 }
