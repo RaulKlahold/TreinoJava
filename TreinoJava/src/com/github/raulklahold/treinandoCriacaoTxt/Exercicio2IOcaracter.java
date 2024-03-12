ab0d019dd7db8fa6f560f56eeb85fb8aace67bca
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-11 22:50:44 -0300

    iniciando o exercicio

--- TreinoJava/src/com/github/raulklahold/treinandoCriacaoTxt/Exercicio2IOcaracter.java
+++ TreinoJava/src/com/github/raulklahold/treinandoCriacaoTxt/Exercicio2IOcaracter.java
@@ -1,5 +1,10 @@
 package com.github.raulklahold.treinandoCriacaoTxt;
 
+import java.io.IOException;
+
 public class Exercicio2IOcaracter {
 
+	public static void lerTecladoEscreverDocumento()throws IOException{
+		System.out.println("Digite 3 recomendações de filmes: ");
+	}
 }
