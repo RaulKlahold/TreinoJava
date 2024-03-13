f593c0e69f7d3e2d1b69fbc5fd0905e8d6c27c34
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-12 21:32:35 -0300

    replicando o projeto

--- TreinoJava/src/com/github/raulklahold/treinoCharacterObj/ExemploIoObjct.java
+++ TreinoJava/src/com/github/raulklahold/treinoCharacterObj/ExemploIoObjct.java
@@ -8,6 +8,12 @@ public class ExemploIoObjct {
 		Carro carro = new Carro("Mustang", 198, 1998, "Preto");
 		
 		File f = new File("Carro");
+		
+		ObjectOutputStream oss = new ObjectOutputStream(new FileOutputStream(f.getName()));
+		
+		oss.writeObject(carro);
+		System.out.printf("Arquivo %s criado com sucesso! Tamanho %d bytes.", f.getName(), f.length());
+		
 	}
 
 }
