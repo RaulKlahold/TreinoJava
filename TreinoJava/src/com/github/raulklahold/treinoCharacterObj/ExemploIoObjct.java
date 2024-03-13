b00a2f4251f92f824f23268bb1af67a2e59ae7a0
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-12 21:28:03 -0300

    deixando o projeto em andamento

--- TreinoJava/src/com/github/raulklahold/treinoCharacterObj/ExemploIoObjct.java
+++ TreinoJava/src/com/github/raulklahold/treinoCharacterObj/ExemploIoObjct.java
@@ -5,7 +5,15 @@ import java.io.*;
 public class ExemploIoObjct {
 	
 	public static void serealizacao() throws IOException{
+		Carro carro = new Carro("Mustang", 198, 1998, "Preto");
 		
+		File f = new File("Carro");
 	}
 
 }
+/*
+private String marca;
+private Integer VelocidadeMax;
+private Integer ano;
+private String cor;
+*/
