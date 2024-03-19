9c99e454a95714fae832c10135f805818bbbd78a
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-18 21:41:30 -0300

    ccriação de getters e setters

--- TreinoJava/src/com/github/raulklahold/treinoSimples/ColetarDados.java
+++ TreinoJava/src/com/github/raulklahold/treinoSimples/ColetarDados.java
@@ -1,6 +1,22 @@
 package com.github.raulklahold.treinoSimples;
 
 public class ColetarDados {
-private double numero1;
-private double numero2;
+	private double numero1;
+	private double numero2;
+
+	public double getNumero1() {
+		return numero1;
+	}
+
+	public void setNumero1(double numero1) {
+		this.numero1 = numero1;
+	}
+
+	public double getNumero2() {
+		return numero2;
+	}
+
+	public void setNumero2(double numero2) {
+		this.numero2 = numero2;
+	}
 }
