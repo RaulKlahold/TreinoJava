f5dfb8d4e04234a5496a9a192b8cfb3bd5751e71
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-18 22:04:43 -0300

    colocando a main para funcionar

--- TreinoJava/src/com/github/raulklahold/treinoSimples/RetornoDaMain.java
+++ TreinoJava/src/com/github/raulklahold/treinoSimples/RetornoDaMain.java
@@ -1,9 +1,25 @@
 package com.github.raulklahold.treinoSimples;
 
+import java.util.Scanner;
+
 public class RetornoDaMain {
 
 	public static void main(String[] args) {
+		ColetarDados dados = new ColetarDados();
+		Scanner scan = new Scanner(System.in);
 		
+		
+	    System.out.println("Digite o numero inicial: ");
+	    double firstN = scan.nextDouble();
+	    dados.setNumero1(firstN);
+	    
+	    System.out.println("Digite um numero final");
+	    double secondN = scan.nextDouble();
+	    dados.setNumero2(secondN);
+	    
+	    double resultante = CalculadoraDeRelação.Relacao(dados);
+	    System.out.println();
+	  
 	}
 
 }
