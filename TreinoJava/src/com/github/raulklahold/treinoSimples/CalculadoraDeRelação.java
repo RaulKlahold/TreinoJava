197889320a06f9f6f77e26d56d668dfd57d6137d
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-18 21:49:39 -0300

    criação do calculo

--- TreinoJava/src/com/github/raulklahold/treinoSimples/CalculadoraDeRelação.java
+++ TreinoJava/src/com/github/raulklahold/treinoSimples/CalculadoraDeRelação.java
@@ -1,5 +1,12 @@
 package com.github.raulklahold.treinoSimples;
 
 public class CalculadoraDeRelação {
+	public CalculadoraDeRelação(ColetarDados ColetarDados) {
+		
+		double primeiraVariavel = ColetarDados.getNumero1();
+		double segundaVariavel = ColetarDados.getNumero2();
+		
+		double relacao = 
+	}
 
 }
