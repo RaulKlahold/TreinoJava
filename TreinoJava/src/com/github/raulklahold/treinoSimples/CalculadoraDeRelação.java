20e0cc9db2803aa6b6807ebea06b6f36baadf053
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-19 21:21:01 -0300

    ajustando

--- TreinoJava/src/com/github/raulklahold/treinoSimples/CalculadoraDeRelação.java
+++ TreinoJava/src/com/github/raulklahold/treinoSimples/CalculadoraDeRelação.java
@@ -1,20 +1,19 @@
 package com.github.raulklahold.treinoSimples;
 
 public class CalculadoraDeRelação {
-	public static double Relacao(ColetarDados ColetarDados) throws ExceptionPersonalizada{
-		
+	public static double Relacao(ColetarDados ColetarDados) throws ExceptionPersonalizada {
+
 		double primeiraVariavel = ColetarDados.getNumero1();
 		double segundaVariavel = ColetarDados.getNumero2();
-		
-		if(primeiraVariavel > 0 && segundaVariavel > 0 ) {
-		
-		 return ((primeiraVariavel - segundaVariavel)/ segundaVariavel) * 100; 
-		}
-		else {
+
+		if (primeiraVariavel > 0 && segundaVariavel > 0) {
+
+			return ((primeiraVariavel - segundaVariavel) / segundaVariavel) * 100;
+		} else {
 			throw new ExceptionPersonalizada();
-			
+
 		}
-		
+
 	}
-      
+
 }
