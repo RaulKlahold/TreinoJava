0aaf44af34e11bfc2b28dda3bfce1695e72e3894
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-20 21:13:29 -0300

    criando a exception personalizada

--- TreinoJava/src/com/github/raulklahold/calculadorasemconnsulta/ExceptionNumber.java
+++ TreinoJava/src/com/github/raulklahold/calculadorasemconnsulta/ExceptionNumber.java
@@ -1,5 +1,13 @@
 package com.github.raulklahold.calculadorasemconnsulta;
 
 public class ExceptionNumber {
+	
+	public ExceptionNumber() {
+		super("O numero precisa ser maior que 0! ");
+	}
+
+	public ExceptionNumber(String message) {
+		super(message);
+	}
 
 }
