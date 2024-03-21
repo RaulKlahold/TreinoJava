5f1f679fcd43a99a7e64e6374b5159bb928dc2d4
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-20 21:22:26 -0300

    teste deu certo

--- TreinoJava/src/com/github/raulklahold/calculadorasemconnsulta/RetornaMain.java
+++ TreinoJava/src/com/github/raulklahold/calculadorasemconnsulta/RetornaMain.java
@@ -1,29 +1,29 @@
 package com.github.raulklahold.calculadorasemconnsulta;
 
 import java.util.Scanner;
- class RetornaMain {
-    
+
+class RetornaMain {
+
 	public static void main(String[] args) throws ExceptionNumber {
-	
+
 		Scanner scan = new Scanner(System.in);
 		ColetarDados cal = new ColetarDados();
-		
+
 		System.out.println("Digite um numero: ");
 		double var1 = scan.nextDouble();
 		cal.setNum1(var1);
-		
+
 		System.out.println("Digite um segundo numero: ");
 		double var2 = scan.nextDouble();
 		cal.setNum2(var2);
 		scan.close();
-		
-       try {
-       Double resultado = ExecutaFunction.soma(cal);
-       System.out.println("O resultado deu: "+resultado);
-       } catch (ExceptionNumber k) {
-    	   System.out.println(k.getMessage());
-       }
-	
-		}}
-	
 
+		try {
+			Double resultado = ExecutaFunction.soma(cal);
+			System.out.println("O resultado deu: " + resultado);
+		} catch (ExceptionNumber k) {
+			System.out.println(k.getMessage());
+		}
+
+	}
+}
