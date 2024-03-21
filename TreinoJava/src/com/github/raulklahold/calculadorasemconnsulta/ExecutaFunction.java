5d6b9d2bb8c88895bae7d716f2468275b808ecb3
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-20 21:22:46 -0300

    refactor 1

--- TreinoJava/src/com/github/raulklahold/calculadorasemconnsulta/ExecutaFunction.java
+++ TreinoJava/src/com/github/raulklahold/calculadorasemconnsulta/ExecutaFunction.java
@@ -1,17 +1,16 @@
 package com.github.raulklahold.calculadorasemconnsulta;
 
 public class ExecutaFunction {
-	public static double soma(ColetarDados ColetarDados) throws ExceptionNumber{
+	public static double soma(ColetarDados ColetarDados) throws ExceptionNumber {
 
 		double numero1 = ColetarDados.getNum1();
 		double numero2 = ColetarDados.getNum2();
-		
-      if( numero1 > 0 && numero2 > 0) {
-		return numero1 + numero2;
-      }
-      else {
-    	  throw new ExceptionNumber();
-      }
+
+		if (numero1 > 0 && numero2 > 0) {
+			return numero1 + numero2;
+		} else {
+			throw new ExceptionNumber();
+		}
 	}
 
 }
