e4c0d3eba35451fbf5289d3db8efdf6312652e61
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-20 21:15:41 -0300

    adição da extensão

--- TreinoJava/src/com/github/raulklahold/calculadorasemconnsulta/ExecutaFunction.java
+++ TreinoJava/src/com/github/raulklahold/calculadorasemconnsulta/ExecutaFunction.java
@@ -1,11 +1,12 @@
 package com.github.raulklahold.calculadorasemconnsulta;
 
 public class ExecutaFunction {
-	public static double soma(ColetarDados ColetarDados) {
+	public static double soma(ColetarDados ColetarDados) throws ExceptionNumber{
 
 		double numero1 = ColetarDados.getNum1();
 		double numero2 = ColetarDados.getNum2();
-
+		
+      
 		return numero1 + numero2;
 
 	}
