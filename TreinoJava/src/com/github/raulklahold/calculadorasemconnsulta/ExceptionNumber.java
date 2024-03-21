e53170079a01fb95121ffded7ff7576ffd75fb95
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-20 21:23:12 -0300

    final refactor

--- TreinoJava/src/com/github/raulklahold/calculadorasemconnsulta/ExceptionNumber.java
+++ TreinoJava/src/com/github/raulklahold/calculadorasemconnsulta/ExceptionNumber.java
@@ -2,7 +2,7 @@ package com.github.raulklahold.calculadorasemconnsulta;
 
 @SuppressWarnings("serial")
 public class ExceptionNumber extends Exception {
-	
+
 	public ExceptionNumber() {
 		super("O numero precisa ser maior que 0! ");
 	}
